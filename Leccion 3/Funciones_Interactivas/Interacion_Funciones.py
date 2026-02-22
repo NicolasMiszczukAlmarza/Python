f5cde522021d60cd7a8522ce811817c99af971fb
NicolasMiszczukAlmarza <nicomovil1994@gmail.com>
2026-02-22 11:32:07 +0100

    Actualiza Interacion_Funciones con nuevas modificaciones

---
+++ Leccion 3/Funciones_Interactivas/Interacion_Funciones.py
@@ -0,0 +1,32 @@
+from random import shuffle
+#Lista inicial
+palitos=['-','--','---','----']
+
+#Mezcla Palitos
+def mezclar(palitos):
+    shuffle(palitos)
+    return palitos
+
+#Pedirle Intento
+def probar_suerte():
+    intento = ''
+    while intento not in ['1','2','3','4']:
+        intento=input(f"Elige un numero entre el 1 y el 4: ")
+    return int(intento)
+
+
+
+#Comprobar intento
+def chequear_intento(lista,intento):
+    if lista[intento - 1] == '-':
+        print("A lavar los platos ")
+    else:
+        print("Esta vez te has salvado ")
+    print(f"Te ha tocado {lista[intento-1]} ")
+
+
+palitos_mezclados=mezclar(palitos)
+seleccion=probar_suerte()
+chequear_intento(palitos_mezclados,seleccion)
+
+
